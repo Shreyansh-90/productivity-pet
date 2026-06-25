f60fc8e6c87b58b5e823bfbd99c4ed1b2c569b76
Shreyansh-90 <physcoim00o@gmail.com>
2026-06-25 18:06:29 +0530

    Profile section redesign (ongoing)

--- app/(protected)/profile/page.tsx
+++ app/(protected)/profile/page.tsx
@@ -16,24 +16,19 @@ const Profile = () => {
     if (!isMounted) return null; // Don't render until we're sure we're on the client
 
   return (
-    <main className= "p-8" >
-        <div>
-            <h1 className="text-3xl font-bold ">Fluffys Profile</h1>
-            <p className="text-lg font-semibold">Name: {pet.name}</p>
-            <input 
-                type='text'
-                value={pet.name}
-                onChange={(e) => updateName(e.target.value)}
-            />
-            <p className="text-lg font-semibold">Age: {pet.age}</p>
-            <p className="text-lg font-semibold">XP: {pet.xp}</p>
-            <p className="text-lg font-semibold">Sleeping Habits: {pet.sleepingHabits}</p>
-            <p className="text-lg font-semibold">Eating Habits: {pet.eatingHabits}</p>
-            <p className="text-lg font-semibold">Evolution Stage: {pet.evolutionStage}</p>
-            <p className="text-lg font-semibold">Allergies: {pet.allergies}</p>
+    <main className="p-6 bg-gray-500/10 flex flex-col rounded-xl top-15 left-auto w-full">
+      <h1 className="text-3xl font-bold flex mb-5 p-2">Profile</h1>
+      <div className="bg-white flex gap-10 flex-auto rounded-2xl p-5 w-full ">
+        <div className="bg-gray-500 rounded-full w-32 h-32 text-center flex">
+          User&apos;s Photo here
         </div>
+        <div className="flex flex-col gap-1">
+          <h1 className='text-xl font-semibold'>{pet.name}</h1>
+          <h1 className='text-sm text-gray-600'>email123@classic.com</h1>
+        </div>
+      </div>
     </main>
-  )
+  );
 }
 
 export default Profile
